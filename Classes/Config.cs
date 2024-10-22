f545b5626502cab7fa59e2e72e6733cfef3b50ac
HP <HP@DESKTOP-T6A34MC>
2024-10-23 01:37:03 +0500

    модель партнёров + класс подключения + контекст для работы с бд. Бугрина

---
+++ Classes/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UP01.Classes
+{
+    public class Config
+    {
+        public static readonly string config = "Server=localhost\\SQLEXPRESS;" +
+             "Trusted_Connection=True;" +
+             "Database=UP01;" +
+             "User=;" +
+             "Pwd=;";
+    }
+}
---
+++ Classes/ContextPartners.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UP01.Classes
+{
+    public class ContextPartners : DbContext
+    {
+        public DbSet<Models.Partners> Partners { get; set; }
+        public ContextPartners() => Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(Config.config);
+    }
+}
